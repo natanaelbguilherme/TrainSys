9c26512cfac6b4267d44731be122898c539581d2
natanaelbguilherme <natanaelbguilherme@gmail.com>
2023-08-29 19:55:47 -0300

    adiciona app bar e msg de erro no login

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
 <template>
-  <router-view />
+  <v-layout>
+    <v-app-bar color="#d6249f" v-if="renderizarMenu">
+      <v-app-bar-title class="titleMenu">TrainSys</v-app-bar-title>
+
+      <v-btn icon>
+        <router-link to="/deashboard"> Home </router-link>
+      </v-btn>
+    </v-app-bar>
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-layout>
 </template>
 
-<script setup>
-  //
+<script>
+export default {
+  computed: {
+    renderizarMenu() {
+      return this.$router.path == "/" && this.$router.path == "/usuario/novo";
+    },
+  },
+};
 </script>
--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -1,10 +1,12 @@
 <template>
-  <div>
+  <div class="container">
     <v-sheet width="300" class="mx-auto">
       <v-form fast-fail @submit.prevent="fazerLogin">
         <v-text-field v-model="email" label="E-mail"></v-text-field>
+        <span v-if="erroEmail">{{ erroEmail }}</span>
 
         <v-text-field v-model="password" label="Senha"></v-text-field>
+        <span v-if="erroSenha">{{ erroSenha }}</span>
 
         <v-btn type="submit" block class="mt-2">Entrar</v-btn>
       </v-form>
@@ -22,11 +24,30 @@ export default {
   data: () => ({
     email: "",
     password: "",
+
+    erroEmail: "",
+    erroSenha: "",
   }),
   methods: {
     fazerLogin() {
       console.log("entrei aqui");
+
+      this.erroEmail = "";
+      this.erroSenha = "";
+
+      if (this.email === "") this.erroEmail = "Digite o email";
+      if (this.password === "") this.erroSenha = "Digite a senha";
+
+      if (this.erroEmail === "" && this.erroSenha === "") {
+        this.$router.push("/deashboard");
+      }
     },
   },
 };
 </script>
+
+<style>
+.container {
+  margin-top: 100px;
+}
+</style>
