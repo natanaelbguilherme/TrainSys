1a8b37bedc68e55a6647ee25d83a180d8e259882
natanaelbguilherme <natanaelbguilherme@gmail.com>
2023-08-28 21:05:43 -0300

    adiciona formulario de login

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -1,3 +1,32 @@
 <template>
-  <h1>Login</h1>
+  <div>
+    <v-sheet width="300" class="mx-auto">
+      <v-form fast-fail @submit.prevent="fazerLogin">
+        <v-text-field v-model="email" label="E-mail"></v-text-field>
+
+        <v-text-field v-model="password" label="Senha"></v-text-field>
+
+        <v-btn type="submit" block class="mt-2">Entrar</v-btn>
+      </v-form>
+      <p>
+        Ainda não tem conta?<router-link to="/usuario/novo"
+          >Cadastre-se</router-link
+        >
+      </p>
+    </v-sheet>
+  </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    email: "",
+    password: "",
+  }),
+  methods: {
+    fazerLogin() {
+      console.log("entrei aqui");
+    },
+  },
+};
+</script>
